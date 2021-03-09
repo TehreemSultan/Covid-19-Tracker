52ef8c70131fd41c9bad824222e16eee027afeb4
TehreemSultan <tehreemsultanali@gmail.com>
2021-03-09 19:52:11 +0500

    cards data fetch and display

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+
+
 import React from 'react'
 import {Cards, Chart, CountryPicker} from './components';
 import styles from './App.module.css';
+import {fetchData} from './api';
 
 class App extends React.Component{
 
+state={
+    data:{}
+}
+
+async componentDidMount(){
+    const fetchedData= await fetchData();
+    this.setState({data: fetchedData})
+    console.log("app",fetchedData);
+}
 render(){
-   
+    const {data}=this.state;
     return (
         <div className={styles.container}>
-           <Cards />
+           <Cards data={data}/>
            <Chart/>
            <CountryPicker/>
         </div>
@@ -16,3 +28,4 @@ render(){
 }}
 
 export default App
+
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import {Card, CardContent, Typography, Grid} from '@material-ui/core';
+import styles from './Cards.module.css';
+import CountUp from 'react-countup';
 
-const Cards = () => {
-  return(
-      <div><h1>Cards</h1></div>
+const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
+    if(!confirmed){
+        return 'loading...'
+    }
+    const data=[
+        {
+            cardData:confirmed,
+            cardHeading:"Infected",
+            cardPara:"number of active cases of covid 19"
+        },
+        {
+            cardData:recovered,
+            cardHeading:"Recovered",
+            cardPara:"number of recoveries from covid 19"
+        },
+        {
+            cardData:deaths, 
+            cardHeading:"Deaths",
+            cardPara:"number of deaths from covid 19"
+        }
+    ];
+    const cards=data.map((item)=>{
+       
+        return(<Grid item component={Card} xs={12} md={3} className={`${styles.card}
+         ${item.cardHeading==='Infected'?styles.Infected:
+         item.cardHeading==='Recovered'?styles.Recovered:styles.Deaths}`}>
+        <CardContent>
+            <Typography color='textSecondary' gutterBottom>{item.cardHeading}</Typography>
+            <Typography variant="h5">
+                <CountUp start={0} end={item.cardData.value} duration={2.5} separator=","/>
+            </Typography>
+            <Typography color='textSecondary'>{new Date(lastUpdate).toDateString()}</Typography>
+            <Typography variant='body2'>{item.cardPara}</Typography>
+        </CardContent>
+    </Grid>
+    )})
+    return (
+        <div className={styles.container}>
+            <Grid container spacing={3} justify='center'>
+            {cards}  
+            </Grid>
+        </div>
     )
 }
 
 export default Cards
+
+
