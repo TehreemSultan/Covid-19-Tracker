c9148121038925aad7e6cc8121ad4153af25f176
TehreemSultan <tehreemsultanali@gmail.com>
2021-03-10 02:59:37 +0500

    select country option and bar chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,18 +3,17 @@ import {fetchDailyData} from '../../api';
 import {Line, Bar} from 'react-chartjs-2';
 import styles from './Chart.module.css'
 
-const Chart = () => {
-    const [dailyData, setDailyData] = useState([{check:"mm"}]);
-
+const Chart = ({data:{confirmed, recovered, deaths}, country}) => {
+    const [dailyData, setDailyData] = useState([]);
+   console.log(confirmed);
     useEffect(()=>{
         const fetchApi= async () =>{
             setDailyData(await fetchDailyData());
-            console.log(dailyData);
         }
      
         fetchApi();
       
-    });
+    },[]);
 
     const lineChart=(
         dailyData.length?
@@ -36,11 +35,33 @@ const Chart = () => {
             }]
         }}
         />):null
-    )
+    );
+ 
+    const barChart=(
+        confirmed?
+        (<Bar
+        data={{
+            labels:['Infected', 'Recovered', 'Deaths'],
+            datasets:[{
+                data:[confirmed.value, recovered.value, deaths.value],
+                label:'People',
+                backgroundColor:['rgba(0, 0, 255, 0.8)', 'rgba(1, 173, 1, 0.8) ', 'rgba(255, 0, 0, 0.8)']
+            }],
+           
+        }}
+        options={{
+            legend:{display:false},
+            title:{display:true, text:`Current state in ${country}`},
+        }}
+        
+        /> ) 
+        :null   
+)
 
     return (
         <div className={styles.container}>
-            {lineChart}
+            {country?barChart:lineChart}
+           
         </div>
     )
 }
--- src/components/CountryPicker/CountryPicker.js
+++ src/components/CountryPicker/CountryPicker.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import {NativeSelect, FormControl} from '@material-ui/core';
+import {fetchCountries} from '../../api';
+import styles from './CountryPicker.module.css';
 
-const CountryPicker = () => {
+
+const CountryPicker = ({handleCountryChange}) => {
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect(()=>{
+        const fetchAPI= async ()=>{
+            setFetchedCountries(await fetchCountries());
+        }
+        fetchAPI();
+    },[setFetchedCountries]);
+
     return (
-        <div>
-            <h1>countrypicker</h1>
-        </div>
+        <FormControl className={styles.formControl}>
+            <NativeSelect defaultValue='' onChange={(e)=>handleCountryChange(e.target.value)} className={styles.formControl}>
+                <option value="">Global</option>
+                {fetchedCountries.map((country,i)=><option key={i} value={country}>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
     )
 }
 
